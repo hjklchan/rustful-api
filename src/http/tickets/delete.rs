20738f665bccc8195d146208d95b3448c5c1c610
Illusion <1161524405@qq.com>
2024-06-23 19:10:21 +0800

    complete delete handler

--- src/http/tickets/delete.rs
+++ src/http/tickets/delete.rs
@@ -1,5 +1,23 @@
-use axum::response::IntoResponse;
+use axum::{
+    extract::{Path, State},
+    response::IntoResponse,
+};
 
-pub async fn delete_handler() -> impl IntoResponse {
-    "Delete"
+use crate::{
+    app_state::AppState,
+    http::{error::ServiceError, response::Response, OhMyResult},
+};
+
+pub async fn delete_handler(
+    State(AppState { ref pool }): State<AppState>,
+    Path(article_id): Path<u64>,
+) -> OhMyResult<Response<()>> {
+    let sql = "UPDATE `articles` SET `deleted_at` = NOW() WHERE `id` = ?";
+
+    sqlx::query(sql)
+        .bind(article_id)
+        .execute(pool)
+        .await
+        .map(|_| OhMyResult::Ok(Response::Ok))
+        .map_err(|err| ServiceError::SqlxError(err))?
 }
