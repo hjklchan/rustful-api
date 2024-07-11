94cc5c755c1496cbdc8fa1535a60b8de2f88a51a
Lucas chen <lucas.chen@gbo-asia.com>
2024-07-11 17:48:40 +0800

    transform codes

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,2 +1,20 @@
-pub mod pagination;
+use axum::response::IntoResponse;
+use reqwest::StatusCode;
+
 pub mod jwt;
+pub mod pagination;
+
+pub struct Codes(StatusCode, &'static str);
+
+impl From<(StatusCode, &'static str)> for Codes {
+    fn from((status_code, code): (StatusCode, &'static str)) -> Self {
+        Self(status_code, code)
+    }
+}
+
+impl IntoResponse for Codes {
+    fn into_response(self) -> axum::response::Response {
+        // todo: transform to (status_code, ErrResponse::new(code, message)).into_response()
+        unimplemented!()
+    }
+}
